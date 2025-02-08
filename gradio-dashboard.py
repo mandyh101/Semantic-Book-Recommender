ad9d5a291b0c4e92863fe9fe2c9de829b9703753
mandy <mandyhale10@gmail.com>
2025-02-09 12:58:36 +1300

    import dependencies for gradio dashboard

---
+++ gradio-dashboard.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_chroma import Chroma
+
+import gradio as gr
+
+load_dotenv()
+
+books = pd.read_csv("books_with_emotions.csv")
+
